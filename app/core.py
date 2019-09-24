a20676d1c7444070d4a91940e65b5b3c675552ed
tomy0000000 <tomy0000000@gmail.com>
2019-09-24 08:47:49 +0000

    Modify deploy scripts

--- app/core.py
+++ app/core.py
@@ -1,4 +1,3 @@
-#! /home/tomy0000000/.local/share/virtualenvs/TSkr-laitjqxb/bin/python
 """
 This is an example showing how to make the scheduler into a remotely accessible service.
 It uses RPyC to set up a service through which the scheduler can be made to add, modify and remove
@@ -36,24 +35,18 @@ class SchedulerService(rpyc.Service):
         logging.info(results)
         return results
     def exposed_configure(self, gconfig={}, prefix="apscheduler.", **options):
-        logging.info("-------------------")
-        logging.info(gconfig)
-        logging.info("-------------------")
-        logging.info(prefix)
-        logging.info("-------------------")
-        logging.info(options)
+        logging.info("TSkr Core Configured: {}".format(options))
         scheduler.configure(gconfig, prefix, **options)
     def exposed_start(self, paused=False):
-        logging.info("TSkr Core Started!!!")
+        logging.info("TSkr Core Started")
         scheduler.start(paused)
 
 import logging
-logging.basicConfig(filename="/var/www/TSkr/log/core.log", level=logging.DEBUG)
+logging.basicConfig(filename="log/core.log", level=logging.DEBUG)
 
 if __name__ == "__main__":
     scheduler = BackgroundScheduler()
-    # scheduler.start()
     protocol_config = {"allow_public_attrs": True}
     server = ThreadedServer(SchedulerService, port=4792, protocol_config=protocol_config)
-    logging.info("TSkr Core Launched!!!")
+    logging.info("TSkr Core Launched")
     server.start()
